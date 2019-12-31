460d9ed50f89f64cc76d63fdb562bbbcbf974a9d
Krog <greg@Krog.local>
2019-12-31 08:22:11 -0700

    upgraded to new version of Xerris.Core and now using the ComparesTo Validation extension

--- test/Xerris.DotNet.Core.Aws.Test/Repository/ScanConditionExtensionsTest.cs
+++ test/Xerris.DotNet.Core.Aws.Test/Repository/ScanConditionExtensionsTest.cs
@@ -21,87 +21,101 @@ namespace Xerris.DotNet.Core.Aws.Test.Repository
         public void Equal()
         {
             var eq = this.Equals<Foo>(x => x.FirstName, angelina.FirstName);
-            AssertEquals(eq,  nameof(Foo.FirstName), ScanOperator.Equal,angelina.FirstName);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.FirstName), ScanOperator.Equal,angelina.FirstName), AssertEquals)
+                .Check();
         }
         
         [Fact]
         public void NotEqual()
         {
             var eq = this.NotEqual<Foo>(x => x.LastName, angelina.LastName);
-            AssertEquals(eq,  nameof(Foo.LastName), ScanOperator.NotEqual,angelina.LastName);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.LastName), ScanOperator.NotEqual, angelina.LastName), AssertEquals)
+                .Check();
         }
         
         [Fact]
         public void Contains()
         {
             var eq = this.Contains<Foo>(x => x.LastName, angelina.LastName);
-            AssertEquals(eq,  nameof(Foo.LastName), ScanOperator.Contains,angelina.LastName);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.LastName), ScanOperator.Contains, angelina.LastName), AssertEquals)
+                .Check();
         }
         
         [Fact]
         public void NotContains()
         {
             var eq = this.NotContains<Foo>(x => x.LastName, angelina.LastName);
-            AssertEquals(eq,  nameof(Foo.LastName), ScanOperator.NotContains,angelina.LastName);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.LastName), ScanOperator.NotContains, angelina.LastName), AssertEquals)
+                .Check();
         }
         
         [Fact]
         public void GreaterThan()
         {
             var eq = this.GreaterThan<Foo>(x => x.Age, angelina.Age);
-            AssertEquals(eq,  nameof(Foo.Age), ScanOperator.GreaterThan,angelina.Age);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.Age), ScanOperator.GreaterThan, angelina.Age), AssertEquals)
+                .Check();
         }
         
         [Fact]
         public void GreaterThanOrEqual()
         {
             var eq = this.GreaterThanOrEqual<Foo>(x => x.Age, angelina.Age);
-            AssertEquals(eq,  nameof(Foo.Age), ScanOperator.GreaterThanOrEqual,angelina.Age);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.Age), ScanOperator.GreaterThanOrEqual, angelina.Age), AssertEquals)
+                .Check();
         }
         
         [Fact]
         public void LessThan()
         {
             var eq = this.LessThan<Foo>(x => x.Age, angelina.Age);
-            AssertEquals(eq,  nameof(Foo.Age), ScanOperator.LessThan,angelina.Age);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.Age), ScanOperator.LessThan, angelina.Age), AssertEquals)
+                .Check();
         }
         
         [Fact]
         public void LessThanOrEqual()
         {
             var eq = this.LessThanOrEqual<Foo>(x => x.Age, angelina.Age);
-            AssertEquals(eq,  nameof(Foo.Age), ScanOperator.LessThanOrEqual,angelina.Age);
+            Validate.Begin()
+                .ComparesTo<ScanCondition>(eq, new ScanCondition(nameof(Foo.Age), ScanOperator.LessThanOrEqual, angelina.Age), AssertEquals)
+                .Check();
         }
 
         [Fact]
-        public void TwoScanConditions()
+        public void MultipleScanConditions()
         {
             var condition = this.Equals<Foo>(x => x.FirstName, angelina.FirstName)
                 .And(this.GreaterThan<Foo>(x => x.Age, angelina.Age))
                 .And(this.NotEqual<Foo>(x => x.LastName, "Pitt")).ToList();
-            
+
             Validate.Begin()
                 .IsNotNull(condition, "condition").Check()
                 .IsNotEmpty(condition, "conditions not empty").Check()
-                .IsEqual(condition.Count(), 3, "found 3 conditions");
-
-            
-            AssertEquals(condition[0], nameof(Foo.FirstName), ScanOperator.Equal, angelina.FirstName);
-            AssertEquals(condition[1], nameof(Foo.Age), ScanOperator.GreaterThan, angelina.Age);
-            AssertEquals(condition[2], nameof(Foo.LastName), ScanOperator.NotEqual, "Pitt");
+                .IsEqual(condition.Count(), 3, "found 3 conditions")
+                .ComparesTo<ScanCondition>(condition[0], new ScanCondition(nameof(Foo.FirstName), ScanOperator.Equal, angelina.FirstName), AssertEquals)
+                .ComparesTo<ScanCondition>(condition[1], new ScanCondition(nameof(Foo.Age), ScanOperator.GreaterThan, angelina.Age), AssertEquals)
+                .ComparesTo<ScanCondition>(condition[2], new ScanCondition(nameof(Foo.LastName), ScanOperator.NotEqual, "Pitt"), AssertEquals)
+                .Check();
         }
 
-        private static void AssertEquals(ScanCondition actual, string propertyName, ScanOperator oper, object value)
+        private static void AssertEquals(Validation validation, ScanCondition actual, ScanCondition expected)
         {
-            Validate.Begin()
-                .IsNotNull(actual, "actual").Check()
+            validation.IsNotNull(actual, "actual").Check()
                 .IsNotNull(actual.Values, "value").Check()
                 .IsNotEmpty(actual.Values, "values not empty").Check()
                 .IsEqual(actual.Values.Length, 1, "values count").Check()
                 //check the value of the scan condition
-                .IsEqual(actual.Operator, oper, "operator")
-                .IsEqual(actual.PropertyName, propertyName, "property name")
-                .IsEqual(actual.Values.First(), value, "actual value")
+                .IsEqual(actual.Operator, expected.Operator, "operator")
+                .IsEqual(actual.PropertyName, expected.PropertyName, "property name")
+                .IsEqual(actual.Values.First(), expected.Values.First(), "actual value")
                 .Check();
         }
     }
