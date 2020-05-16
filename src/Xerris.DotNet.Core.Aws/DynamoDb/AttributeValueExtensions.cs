35a4c90bb904600418096016e7a98b47e83fb8b5
Greg Cook <greg@xerris.com>
2020-05-16 16:04:17 -0600

    mapper for AttributeValue dynamodb

--- src/Xerris.DotNet.Core.Aws/DynamoDb/AttributeValueExtensions.cs
+++ src/Xerris.DotNet.Core.Aws/DynamoDb/AttributeValueExtensions.cs
@@ -1,11 +1,59 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Amazon.DynamoDBv2.Model;
 using Xerris.DotNet.Core.Extensions;
+using Xerris.DotNet.Core.Validations;
 
 namespace Xerris.DotNet.Core.Aws.DynamoDb
 {
     public static class AttributeValueExtensions
     {
+        public static string Str(this IReadOnlyDictionary<string, AttributeValue> value, string key, bool enforce = true)
+        {
+            if (!enforce) 
+                return value.ContainsKey(key) ? value[key].S ?? string.Empty : string.Empty;
+            
+            Validate.Begin().IsTrue(value.ContainsKey(key), "key not found").Check()
+                    .IsNotEmpty(value[key].S, $"value at {key} is empty")
+                    .Check();
+            return value[key].S;
+        }
+
+        public static T EnumStr<T>(this IReadOnlyDictionary<string, AttributeValue> value, string key) where T : struct, IConvertible
+        {
+            return value[key].S.ToEnum<T>();
+        }
+
+        public static T Enum<T>(this IReadOnlyDictionary<string, AttributeValue> value, string key) where T : struct, IConvertible
+        {
+            return value[key].N.ToEnum<T>();
+        }
+
+        public static DateTime DateTime(this IReadOnlyDictionary<string, AttributeValue> value, string key)
+        {
+            return value[key].ToDateTime();
+        }
+
+        public static IEnumerable<T> FromList<T>(this Dictionary<string, AttributeValue> value, string key,
+            Func<IReadOnlyDictionary<string, AttributeValue>, T> converter)
+        {
+            return value[key].L.Select(x => converter(x.M));
+        }
+
+        private static T Map<T>(this IReadOnlyDictionary<string, AttributeValue> value, string key, 
+                        Func<IReadOnlyDictionary<string, AttributeValue>, T> converter, bool enforce = false)
+        {
+            var item = value.ContainsKey(key) ? value[key] : null;
+            if (!enforce && item == null) return default;
+            
+            Validate.Begin()
+                    .IsNotNull(item.M, "item is null").Check()
+                    .IsNotEmpty(item.M, $"value at {key} is empty")
+                .Check();
+            return converter(item.M);
+        }
+
         public static T Convert<T>(this AttributeValue value, Func<AttributeValue, T> converter)
         {
             return converter(value);
@@ -13,7 +61,7 @@ namespace Xerris.DotNet.Core.Aws.DynamoDb
 
         public static DateTime ToDateTime(this AttributeValue value)
         {
-            return value.Convert(x => DateTime.Parse(x.S));
+            return System.DateTime.Parse(value.S);
         }
 
         public static T ToEnum<T>(this AttributeValue value) where T : struct, IConvertible
